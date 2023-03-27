344f6e9ec41fa2e688d62fc5eb6e6090f7faeb6c
AranovskyAlex <alexaranovskyi@gmail.com>
2023-03-28 00:14:57 +0300

    Task 6

---
+++ lesson 6/6_6.js
@@ -0,0 +1,27 @@
+let car = {
+name: 'BMW',
+model: 'M5 E60',
+topSpeed: '360 km/h',
+year: 2007,
+drive: 'RW drive',
+defaultHP: '500hp',
+modedHP: '850hp',
+speedTest() {
+console.log(`${this.name} ${this.model} ${this.year} top speed is ${this.topSpeed}`);
+},
+driveInfo() {
+console.log(`In year ${this.year} all ${this.model} had ${this.drive}`);
+},
+improve(){
+console.log(`Stock ${this.model} has ${this.defaultHP}, but you can increase it up to ${this.modedHP}  `);
+}
+} 
+
+setTimeout (() => car.speedTest(), 1000);
+
+let securedDriveInfo = () => car.driveInfo(); 
+setTimeout(securedDriveInfo, 2000);
+let securedImprove = car.improve.bind(car);
+setTimeout(securedImprove, 3000);
+
+
